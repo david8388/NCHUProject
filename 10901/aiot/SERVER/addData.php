c8881700a08ebf71a8da6eaa23f32642abf22858
David <hsienwu830808@gmail.com>
2020-09-23 23:42:53 +0800

    [10901-AIOT] SERVER: add addData.php

---
+++ 10901/aiot/SERVER/addData.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "172.16.100.134";
+$username = "test123";
+$password = "test123";
+$dbname = "aiot";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO sensors (value) VALUES (100)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
