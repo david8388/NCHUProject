225a4733550d39f9209f3be12dbfbe3d9f4127a8
David <hsienwu830808@gmail.com>
2020-10-01 00:09:57 +0800

    [10901-AIOT] HOMEWORK00: set allow-origin all in GetData.php

--- 10901/aiot/HOMEWORK00/GetData.php
+++ 10901/aiot/HOMEWORK00/GetData.php
@@ -1,7 +1,7 @@
 <?php
 
     // Cross-Origin Resource Sharing Header
-    //header('Access-Control-Allow-Origin: http://x.x.x.x');
+    header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
 
